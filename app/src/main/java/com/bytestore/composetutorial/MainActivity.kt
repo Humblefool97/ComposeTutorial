a699910972d4a9dc601644ab48c9c073c4ba71a5
rajeevranganathan <rajeevkr878@gmail.com>
2021-08-02 23:19:05 +0530

    Add Simple text

--- app/src/main/java/com/bytestore/composetutorial/MainActivity.kt
+++ app/src/main/java/com/bytestore/composetutorial/MainActivity.kt
@@ -4,12 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hello Compose!")
+            MessageCard(name = "Compose!")
         }
+    }
+
+    @Composable
+    private fun MessageCard(name: String) {
+        Text("Hellow $name")
+    }
+
+    @Preview
+    @Composable
+    fun PreviewMessageCard() {
+        MessageCard(name = "Android")
     }
 }
